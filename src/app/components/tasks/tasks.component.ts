3d1131515a4cb6680e9f6b8c23dd5c841a014b1c
demarque <fsdemarque@gmail.com>
2025-01-03 13:20:16 -0300

    pequenos ajustes de imports

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { TaskService } from '../../services/task.service';
 import { Tarefa } from '../../../tarefa';
 import { CommonModule } from '@angular/common';
@@ -6,12 +6,13 @@ import { TasksItemComponent } from "../task-item/tasks-item.component";
 
 @Component({
   selector: 'app-tasks',
-  imports: [CommonModule, TasksItemComponent, TasksItemComponent],
+  imports: [CommonModule, TasksItemComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent implements OnInit {
 
+  @Input() tarefa: any;
   tarefas: Tarefa[] = [];
   constructor(private taskService:TaskService) { }
   ngOnInit(): void {
