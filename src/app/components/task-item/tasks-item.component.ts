a6a46db398ac3c2eb4711e382c6d6c6a75ffdf6a
demarque <fsdemarque@gmail.com>
2025-01-03 02:35:55 -0300

    dia 2

---
+++ src/app/components/task-item/tasks-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks-item',
+  imports: [],
+  templateUrl: './tasks-item.component.html',
+  styleUrl: './tasks-item.component.css'
+})
+export class TasksItemComponent {
+
+}
